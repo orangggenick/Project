7588fa29b3cdd5f916c9255c7a542ffdeb3d1a2c
nikitaisakov <orangggenick@mail.ru>
2016-06-19 23:24:45 +0600

    Изменено место хранения статики

--- Project/settings.py
+++ Project/settings.py
@@ -104,10 +104,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
 
-MEDIA_URL = "/images/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
